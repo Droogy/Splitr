4e83baea67c0415a7dbe3077cd3f571c3b144d35
Droogy <drooogy@pm.me>
2021-06-02 16:21:01 -0400

    Added theTuple() routine

--- splitr.py
+++ splitr.py
@@ -10,6 +10,8 @@ To split a file - ./splitr.py file.png
 To rejoin a file - ./split.py -m
 '''
 import sys
+import itertools
+import random
 
 def splitr():
     with open(sys.argv[1], "rb") as mainFile:
@@ -18,30 +20,48 @@ def splitr():
             # create 2 files which will each hold part of main file
             fileOne = open("thing1", "ab+")
             fileTwo = open("thing2", "ab+")
-            # check if byte is divisible by two, if it is, then we just store-
-            # x-1 in one file, and \x01 in the other
-            if(mainFileByte[0] % 2 != 0):
-                byte2write = mainFileByte[0] - 1
-                # note the brackets around "byte2write" avoid a weird null-byte problem
-                fileOne.write(bytes([byte2write]))
-                fileTwo.write(b"\x01")
+            # need to handle ints 0-2 because they mess up our lists
+            if(mainFileByte[0] == 0):
+                fileOne.write(b"\x00")
+                fileTwo.write(b"\x00")
                 mainFileByte = mainFile.read(1)
-            # if our byte is just 1, store 1 in one file and zero in the other
-            elif mainFileByte == b"\x01":
+            elif(mainFileByte[0] == 1):
                 fileOne.write(b"\x01")
                 fileTwo.write(b"\x00")
                 mainFileByte = mainFile.read(1)
-            # if our file is divisible by two, store each half in each file
-            else:
-                byte2write = mainFileByte[0] // 2
-                fileOne.write(bytes([byte2write]))
-                fileTwo.write(bytes([byte2write]))
+            elif(mainFileByte[0] == 2 ):
+                fileOne.write(b"\x00")
+                fileTwo.write(b"\x02")  
+                mainFileByte = mainFile.read(1) 
+            else: # here is our important routine that reads most of the file
+                final = theTupler(mainFileByte)
+                byteOne = final[0]
+                byteTwo = final[1]
+                fileOne.write(bytes([byteOne]))
+                fileTwo.write(bytes([byteTwo]))
                 mainFileByte = mainFile.read(1)
-    # be polite and close your files!
-    mainFile.close()
-    fileOne.close()
-    fileTwo.close()
 
+
+def theTupler(mainByte):
+    factors = []    # list containing every positive number less than our num
+    goodPairs = []   # contains the tuples that add up to our int
+    byteMe = mainByte[0]    # initializes our current byte as an integer
+    
+    while byteMe != 0:
+        byteMe -= 1
+        factors.append(byteMe)
+    # itertools does the hard work here and creates a list of tuples
+    raw_pairs = itertools.permutations(factors, 2)
+
+    # this routine checks if our tuples add up to the given byte 
+    for tuple in raw_pairs:
+        if (tuple[0] + tuple[1]) == (factors[0]+1):
+            goodPairs.append(tuple)
+    
+    final = random.choice(goodPairs)     # select a random tuple
+    return final    # return a tuple which sums our byte
+
+# this routine is ezpz, just puts everything back together!
 def unSplitr():
     with open("thing1", "rb") as fileOne:
         with open("thing2", "rb") as fileTwo:
@@ -53,9 +73,6 @@ def unSplitr():
                     finalByte = final.write(bytes([adder]))
                     byteOne = fileOne.read(1)
                     byteTwo = fileTwo.read(1)
-    fileOne.close()
-    fileTwo.close()
-    final.close()
 
 def main():
     # check if flag is set to merge, if not, run the split routine
